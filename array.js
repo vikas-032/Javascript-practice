c2dfcaa8242b63a2c7bfa72f1c268e73a89f145a
Vikas <7014.vikas@gmail.com>
2024-08-22 19:58:26 +0530

    second commit

--- array.js
+++ array.js
@@ -24,15 +24,15 @@
 //provide function.
 //Example: To demonstrate the use of the array some() method.
 
-function isGreaterThan5(element,index, array){
-    return element > 5;
-}
+// function isGreaterThan5(element,index, array){
+//     return element > 5;
+// }
 
-function func(){
-    let array = [2,5,8,1,4];
-    let value =array.some(isGreaterThan5);
-    console.log(value);
+// function func(){
+//     let array = [2,5,8,1,4];
+//     let value =array.some(isGreaterThan5);
+//     console.log(value);
     
-}
- func();
+// }
+//  func();
 
