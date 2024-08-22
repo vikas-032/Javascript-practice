c51b4ceca5d715ebdb73b3f6a8964da331889277
Vikas <7014.vikas@gmail.com>
2024-08-22 10:11:44 +0530

    second commit

--- array.js
+++ array.js
@@ -13,7 +13,26 @@
 
 // first element
 
-let courses=['math','english','science','hindi','gk'];
-let firstItem = courses[4];
-console.log("first Item:" , firstItem);
+// let courses=['math','english','science','hindi','gk'];
+// let firstItem = courses[4];
+// console.log("first Item:" , firstItem);
+
+
+//Best-Known Javascript Array methods
+//Q.. Array some() Method
+//The array some methon checks if at least one element of the array satisfies the condition implemented by the 
+//provide function.
+//Example: To demonstrate the use of the array some() method.
+
+function isGreaterThan5(element,index, array){
+    return element > 5;
+}
+
+function func(){
+    let array = [2,5,8,1,4];
+    let value =array.some(isGreaterThan5);
+    console.log(value);
+    
+}
+ func();
 
