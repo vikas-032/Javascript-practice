abffd05e12b7aa41a2eb20db3d62d08b539a4c33
Vikas <7014.vikas@example.com>
2024-08-27 22:43:36 +0530

    path node

--- osmodule.js
+++ osmodule.js
@@ -2,9 +2,6 @@ const os = require('node:os');
 
 console.log(os.freemem())
 console.log(os.homedir())
-console.log(os.hostname())
-console.log(os.platform())
-console.log(os.totalmem())
 
 
 
---
+++ pathmodule.js
@@ -0,0 +1,10 @@
+const path = require('node:path'); 
+
+
+const a1= path.basename('C:\\temp\\myfile.html');
+// Returns: 'myfile.html'
+const a2= path.dirname('C:\\temp\\myfile.html');
+
+console.log(a1) 
+console.log(a2) 
+
