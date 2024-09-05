aebbc18ead71262ba0dc28676615b9eeeb4c3a45
Vikas <7014.vikas@gmail.com>
2024-09-05 09:39:20 +0530

    fetching API

--- oop.js
+++ oop.js
@@ -258,5 +258,11 @@
 //   developer: true,
 // };
 
+const URL = "https://cat-fact.herokuapp.com/facts";
 
-   
+const getFacts = async () => {
+  console.log("getting data .....");
+  let response = await fetch(URL);
+  console.log(response);
+};
+ 
