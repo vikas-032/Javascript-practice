eb95738e7ea64df552c8662628f1532758028fcf
Vikas <7014.vikas@gmail.com>
2024-09-06 16:54:44 +0530

    API fetched

--- oop.js
+++ oop.js
@@ -262,7 +262,6 @@ const URL = "https://cat-fact.herokuapp.com/facts";
 
 const getFacts = async () => {
   console.log("getting data .....");
-  let response = await fetch(URL);
-  console.log(response);
+let response = fetch(URL);
+console.log(response);
 };
- 
